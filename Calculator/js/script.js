728d55d3bacf03c13a0810b29ff898d3b8fb722d
Awele123 <nwabuzorawele42@gmail.com>
2024-11-01 18:36:47 +0100

    added portfolio file and calculator file

---
+++ Calculator/js/script.js
@@ -0,0 +1,57 @@
+document.querySelector("#clear").addEventListener("click", ()=>{
+    document.querySelector("#display").value = " ";
+    
+})
+
+document.querySelector("#one").addEventListener("click", ()=>{
+    document.querySelector("#display").value += "1";
+    
+})
+document.querySelector("#two").addEventListener("click", ()=>{
+    document.querySelector("#display").value += "2";
+})
+document.querySelector("#three").addEventListener("click", ()=>{
+    document.querySelector("#display").value += "3";
+})
+document.querySelector("#four").addEventListener("click", ()=>{
+    document.querySelector("#display").value += "4";
+})
+document.querySelector("#five").addEventListener("click", ()=>{
+    document.querySelector("#display").value += "5";
+})
+document.querySelector("#six").addEventListener("click", ()=>{
+    document.querySelector("#display").value += "6";
+})
+document.querySelector("#seven").addEventListener("click", ()=>{
+    document.querySelector("#display").value += "7";
+})
+document.querySelector("#eight").addEventListener("click", ()=>{
+    document.querySelector("#display").value += "8";
+})
+document.querySelector("#nine").addEventListener("click", ()=>{
+    document.querySelector("#display").value += "9";
+})
+document.querySelector("#dot").addEventListener("click", ()=>{
+    document.querySelector("#display").value += ".";
+})
+document.querySelector("#percent").addEventListener("click", ()=>{
+    document.querySelector("#display").value += "%";
+})
+document.querySelector("#multiply").addEventListener("click", ()=>{
+    document.querySelector("#display").value += "*";
+})
+document.querySelector("#add").addEventListener("click", ()=>{
+    document.querySelector("#display").value += "+";
+})
+document.querySelector("#subtract").addEventListener("click", ()=>{
+    document.querySelector("#display").value += "-";
+})
+document.querySelector("#divide").addEventListener("click", ()=>{
+    document.querySelector("#display").value += "/";
+})
+
+
+document.querySelector("#zero").addEventListener("click", ()=>{
+    document.querySelector("#display").value += "0";
+})
+
